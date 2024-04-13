40ee1d3ebc80a4733ea3e60435574dd017b6a924
Daria Sakulina <evon620c@bk.ru>
2024-04-13 22:38:01 +0300

    done

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 13676; // стоимость билета
+        int rub = 20; // количество рублей для одной бонусной мили
+        int bonus = amount / rub; // количество бонусных миль
+
+
+        System.out.println(bonus);
+
+
+    }
+}
